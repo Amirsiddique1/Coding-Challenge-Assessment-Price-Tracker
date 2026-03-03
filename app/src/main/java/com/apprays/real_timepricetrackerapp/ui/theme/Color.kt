e9349b2dbb9a46cc0766342d26a368559cc81458
Shariq-AppRays <shariq.shmeer@apprays.io>
2026-03-03 15:33:33 +0500

    Configure app theme and update MainActivity setup

--- app/src/main/java/com/apprays/real_timepricetrackerapp/ui/theme/Color.kt
+++ app/src/main/java/com/apprays/real_timepricetrackerapp/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.apprays.real_timepricetrackerapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light theme colors
+val LightPrimary = Color(0xFF1565C0)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightBackground = Color(0xFFF5F5F5)
+val LightSurface = Color(0xFFFFFFFF)
+val LightOnBackground = Color(0xFF1A1A1A)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark theme colors
+val DarkPrimary = Color(0xFF90CAF9)
+val DarkOnPrimary = Color(0xFF003366)
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkOnBackground = Color(0xFFE0E0E0)
